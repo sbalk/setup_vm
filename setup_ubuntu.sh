20136b562d3e620973607b2b582f926787f751da
stijn <stijnrest@protonmail.com>
2024-09-27 01:50:22 +0200

    fix mm path

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -25,14 +25,14 @@ echo 'Adding aliases to ~/.zshrc...'
 cat << 'EOF' >> ~/.zshrc
 
 # >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE='~/.local/bin/micromamba';
-export MAMBA_ROOT_PREFIX='~/micromamba';
+# !! Contents within this block are managed by 'micromamba shell init' !!
+export MAMBA_EXE='/root/.local/bin/micromamba';
+export MAMBA_ROOT_PREFIX='/root/micromamba';
 __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
 else
-    alias micromamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
 fi
 unset __mamba_setup
 # <<< mamba initialize <<<
