b64c32815f3720a0138407ba7a976b99ca0bb6a8
stijn <stijnrest@protonmail.com>
2024-09-27 19:17:10 +0200

    install ffmpeg

--- setup_ubuntu_1_zsh.sh
+++ setup_ubuntu_1_zsh.sh
@@ -4,6 +4,7 @@
 echo 'Installing zsh and nano...'
 sudo apt-get install zsh -y
 sudo apt-get install nano -y
+sudo apt-get install ffmpeg -y
 
 echo 'Changing default shell to zsh...'
 chsh -s $(which zsh)
